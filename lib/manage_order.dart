d432a934bcaed61cf2f1818bba4b87fad38233eb
ds0310825 <34474404+ds0310825@users.noreply.github.com>
2019-11-26 15:22:59 +0800

    6th test commit

--- lib/manage_order.dart
+++ lib/manage_order.dart
@@ -38,144 +38,209 @@ Widget manage_order_page (BuildContext context) {
   );
 }
 
-class set_order_page extends StatelessWidget {
+class set_order_page extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return _set_order_page_state();
+  }
+
+}
+
+
+
+class _set_order_page_state extends State<StatefulWidget> {
+
+  final _password_controller = new TextEditingController();
+  final _order_name_controller = new TextEditingController();
+  String menu_name = "選擇";
+  String _order_name_input_error = "訂單名";
+  var _order_name_color = Colors.grey.shade500;
+  String _password_input_error = "密碼";
+  var _password_color = Colors.grey.shade500;
+
+  void _order_name_empty () {
+    setState(() {
+      _order_name_input_error = "訂單名不能為空";
+      _order_name_color = Colors.red.shade500;
+    });
+  }
+  void _order_name_typed () {
+    setState(() {
+      _order_name_input_error = "訂單名";
+      _order_name_color = Colors.grey.shade500;
+    });
+  }
+  void _password_empty () {
+    setState(() {
+      _password_input_error = "密碼不能為空";
+      _password_color = Colors.red.shade500;
+    });
+  }
+  void _password_typed() {
+    setState(() {
+      _password_input_error = "密碼";
+      _password_color = Colors.grey.shade500;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final order_name_controller = TextEditingController();
-    final menu_name_controller = TextEditingController();
-    final password_controller = TextEditingController();
-    String menu_name = "選擇";
 
     return new Scaffold(
-      appBar: AppBar(
-        title: Text('創建訂單'),
-      ),
-      body: new Center(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(bottom: 15),
-                child: Row(
-                  children: <Widget>[
-                    new Text(
-                      "訂單名",
-                      style: TextStyle(
-                          fontSize: 25
-                      ),
-                    ),
-                    Container(
-                      width: 245,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 5),
-                        child: new TextField(
-                            style: TextStyle(
-                              fontSize: 25,
-                            ),
-                            controller: order_name_controller,
-                            decoration: new InputDecoration(
-                              border: InputBorder.none,
-                              filled: true,
-
-                              contentPadding: new EdgeInsets.all(5),
-                              hintText: "訂單名",
-                              hintStyle: new TextStyle(
-                                  color: Colors.grey.shade500,
-                                  fontSize: 25),
-
-
-                            )
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 15),
-                child: Row(
-                  children: <Widget>[
-                    new Text(
-                      "菜單",
-                      style: TextStyle(
-                        fontSize: 25,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 30),
-                      child: new RaisedButton(
-                        child: Text(
-                          menu_name,
+        appBar: AppBar(
+          title: Text('創建訂單'),
+        ),
+        body: new Center(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: Row(
+                      children: <Widget>[
+                        new Text(
+                          "訂單名",
                           style: TextStyle(
-                            color: Colors.grey.shade500,
-                            fontSize: 25,
+                              fontSize: 25
                           ),
                         ),
-                        onPressed: () {
-                          print("selecting menu");
-
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 15),
-                child: Row(
-                  children: <Widget>[
-                    new Text(
-                      "密碼",
-                      style: TextStyle(
-                        fontSize: 25,
-                      ),
-                    ),
-                    Container(
-                        width: 270,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 30),
-                          child: new TextField(
+                        Container(
+                          width: 245,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 5),
+                            child: new TextFormField(
                               style: TextStyle(
                                 fontSize: 25,
                               ),
-                              controller: password_controller,
+                              controller: _order_name_controller,
                               decoration: new InputDecoration(
+                                border: InputBorder.none,
+                                filled: true,
+                                contentPadding: new EdgeInsets.all(5),
+                                labelText: _order_name_input_error,
+                                labelStyle: new TextStyle(
+                                    color: _order_name_color,
+                                    fontSize: 17),
+                              ),
+                              autofocus: true,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: Row(
+                      children: <Widget>[
+                        new Text(
+                          "菜單",
+                          style: TextStyle(
+                            fontSize: 25,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 30),
+                          child: new RaisedButton(
+                            child: Text(
+                              menu_name,
+                              style: TextStyle(
+                                color: Colors.grey.shade500,
+                                fontSize: 25,
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => choose_menu_page()
+                                )
+                              );
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 15),
+                    child: Row(
+                      children: <Widget>[
+                        new Text(
+                          "密碼",
+                          style: TextStyle(
+                            fontSize: 25,
+                          ),
+                        ),
+                        Container(
+                            width: 270,
+                            child: Padding(
+                              padding: EdgeInsets.only(left: 30),
+                              child: new TextField(
+                                style: TextStyle(
+                                  fontSize: 25,
+                                ),
+                                controller: _password_controller,
+                                decoration: new InputDecoration(
                                   border: InputBorder.none,
                                   filled: true,
                                   contentPadding: new EdgeInsets.all(5),
-                                  hintText: "密碼",
-                                  hintStyle: new TextStyle(
-                                      color: Colors.grey.shade500,
-                                      fontSize: 25)
+                                  labelText: _password_input_error,
+                                  labelStyle: new TextStyle(
+                                      color: _password_color,
+                                      fontSize: 17),
 
-                              )
-                          ),
+                                ),
+//                                autofocus: true,
+                              ),
+                            )
                         )
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                child: Center(
-                  child: RaisedButton(
-                    onPressed: (){
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: Center(
+                      child: RaisedButton(
+                        onPressed: (){
+                          if(_order_name_controller.text != ""
+                              &&
+                              _password_controller.text != ""){
 
-                    },
-                    child: Text(
-                      "創建",
-                      style: TextStyle(
-                          fontSize: 25
+                            print("selecting menu");
+//                            Navigator.push(
+//                                context,
+//                                MaterialPageRoute(
+//                                    builder: (context) => choose_menu_page()
+//                                )
+//                            );
+                          }
+                          if(_order_name_controller.text == ""){
+                            _order_name_empty();
+                          }else{
+                            _order_name_typed();
+                          }
+                          if(_password_controller.text == ""){
+                            _password_empty();
+                          }else{
+                            _password_typed();
+                          }
+                        },
+                        child: Text(
+                          "創建",
+                          style: TextStyle(
+                              fontSize: 25
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
 
-            ],
-          ),
+                ],
+              ),
+            )
         )
-      )
     );
   }
 }
@@ -219,6 +284,30 @@ Widget stream_build(BuildContext context) {
 }
 
 
+void showWindow (BuildContext context) async {
+  return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Container(
+            height: 100,
+            width: 100,
+
+
+            // 跳出打名字的視窗
+            child: AlertDialog(
+                title: Text("格式錯誤"),
+                content: Column(
+                    children: <Widget>[
+                      Text("test")
+
+                    ]
+                )
+            )
+        );
+      }
+  );
+}
 //Widget stream_build(BuildContext context) {
 //  return new StreamBuilder(
 //      stream: Firestore.instance.collection('Users')
